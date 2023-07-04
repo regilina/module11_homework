615e88a2b59f69b6d1d374f0837a37c209e80fcc
Regi <gt12139@gmail.com>
2023-07-04 22:55:24 +0300

    Task_4 completed.

---
+++ task_4.js
@@ -0,0 +1,14 @@
+function printNumbers(start, end) {
+    let current = start;
+  
+    const intervalId = setInterval(() => {
+      console.log(current);
+      if (current === end) {
+        clearInterval(intervalId);
+      }
+      current++;
+    }, 1000);
+  }
+  
+  // Пример использования:
+  printNumbers(5, 15);
