ebf32b002279b6b5e3d8f42f7f46a1f1a6187c8a
Regi <gt12139@gmail.com>
2023-07-06 02:13:39 +0300

    Code style changed.

--- task_1.js
+++ task_1.js
@@ -1,27 +1,26 @@
-function countEvenOddZeroElements(arr) {
-    let evenCount = 0;
-    let oddCount = 0;
-    let zeroCount = 0;
-  
-    for (let i = 0; i < arr.length; i++) {
-      const element = arr[i];
-  
-      if (typeof element === 'number' && !isNaN(element)) {
-        if (element === 0) {
-          zeroCount++;
-        } else if (element % 2 === 0) {
-          evenCount++;
-        } else {
-          oddCount++;
-        }
+function countEvenOddZeroElements (arr) {
+  let evenCount = 0
+  let oddCount = 0
+  let zeroCount = 0
+
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i]
+
+    if (typeof element === 'number' && !isNaN(element)) {
+      if (element === 0) {
+        zeroCount++
+      } else if (element % 2 === 0) {
+        evenCount++
+      } else {
+        oddCount++
       }
     }
-  
-    console.log('Количество чётных элементов: ' + evenCount);
-    console.log('Количество нечётных элементов: ' + oddCount);
-    console.log('Количество нулевых элементов: ' + zeroCount);
   }
-  
-  const array = [1, 2, 3, 0, 4, 5, 'a', null, undefined];
-  countEvenOddZeroElements(array);
-  
+
+  console.log('Количество чётных элементов: ' + evenCount)
+  console.log('Количество нечётных элементов: ' + oddCount)
+  console.log('Количество нулевых элементов: ' + zeroCount)
+}
+
+const array = [1, 2, 3, 0, 4, 5, 'a', null, undefined]
+countEvenOddZeroElements(array)
--- task_2.js
+++ task_2.js
@@ -1,26 +1,26 @@
-function checkPrimeNumber(number) {
-    if (number <= 1 || number > 1000) {
-      console.log("Данные неверны");
-      return;
-    }
-  
-    let isPrime = true;
-  
-    for (let i = 2; i < Math.sqrt(number); i++) {
-      if (number % i === 0) {
-        isPrime = false;
-        break;
-      }
-    }
-  
-    if (isPrime) {
-      console.log("Простое число");
-    } else {
-      console.log("Составное число");
+function checkPrimeNumber (number) {
+  if (number <= 1 || number > 1000) {
+    console.log('Данные неверны')
+    return
+  }
+
+  let isPrime = true
+
+  for (let i = 2; i < Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      isPrime = false
+      break
     }
   }
-  
-  // Пример использования
-  checkPrimeNumber(17); // Простое число
-  checkPrimeNumber(21); // Составное число
-  checkPrimeNumber(1001); // Данные неверны
+
+  if (isPrime) {
+    console.log('Простое число')
+  } else {
+    console.log('Составное число')
+  }
+}
+
+// Пример использования
+checkPrimeNumber(17) // Простое число
+checkPrimeNumber(21) // Составное число
+checkPrimeNumber(1001) // Данные неверны
