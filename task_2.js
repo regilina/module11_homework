0ef413170bbd01ca303169ebb1225c0db9c483cb
Regi <gt12139@gmail.com>
2023-07-05 17:35:33 +0300

    Task_2 optimized.

--- task_2.js
+++ task_2.js
@@ -6,7 +6,7 @@ function checkPrimeNumber(number) {
   
     let isPrime = true;
   
-    for (let i = 2; i < number; i++) {
+    for (let i = 2; i < Math.sqrt(number); i++) {
       if (number % i === 0) {
         isPrime = false;
         break;
